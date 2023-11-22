05186863d11032f3d91cfd30db496ad3a2955f38
Antonino Cangialosi <antonino.cangialosi@agilelab.it>
2023-11-22 10:39:51 +0100

    Finish exercise 00

--- jira-wip/src/path_to_enlightenment.rs
+++ jira-wip/src/path_to_enlightenment.rs
@@ -1,1 +1,7 @@
 // This file is handled by `koans-framework` so you will not need to modify it manually.
+#[path = "koans/00_greetings/00_greetings.rs"]
+mod greetings;
+
+#[path = "koans/01_ticket/01_ticket.rs"]
+mod ticket;
+
